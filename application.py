b99a4da0909694e2a3ee90fe40805f648953e14d
7shiroi <lucky7kelvin@yahoo.com>
2020-08-07 20:27:19 +0800

    Update application.py

--- application.py
+++ application.py
@@ -19,11 +19,11 @@ dbcon = ''
 if ENV == 'DEV':
     app.debug = True
     app.config['SQLALCHEMY_ECHO'] = True
-    dbcon = 'postgresql://postgres:N3wd0r14@127.0.0.1/project1'
+    dbcon = 'postgres-local'
 else:
     app.debug = False
     app.config['SQLALCHEMY_ECHO'] = False
-    dbcon = 'postgres://ykazctrurcyqki:e0140a8b8637ee5b22906c588e53b7445d630d2736f52c43f3e718aade5f8f13@ec2-54-81-37-115.compute-1.amazonaws.com:5432/ddp1qc9fuitp0j'
+    dbcon = 'postgres-heroku'
 app.config['SQLALCHEMY_DATABASE_URI'] = dbcon
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
